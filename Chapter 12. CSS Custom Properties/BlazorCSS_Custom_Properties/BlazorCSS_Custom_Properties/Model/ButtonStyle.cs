dba3a9ffa668f36e8dbf4df1dcb73501e791384e
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2023-08-04 17:52:58 +0530

    Chapter 12

---
+++ Chapter 12. CSS Custom Properties/BlazorCSS_Custom_Properties/BlazorCSS_Custom_Properties/Model/ButtonStyle.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorCSS_Custom_Properties.Model
+{
+    public class ButtonStyle
+    {
+        public string Height { get; set; }
+
+        public string Width { get; set; }
+
+        public string FontSize { get; set; }
+
+        public string ForegroundColor { get; set; }
+
+        public string BackgroundColor { get; set; }
+
+        public string Content { get; set; }
+    }
+}
