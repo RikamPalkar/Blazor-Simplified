387458a15696703a8735db909da0b7ccb29ca488
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2023-08-04 17:48:11 +0530

    Chapter 13. Event call backs

---
+++ Chapter 13. Event call-backs/EventCall/Data/Models/SmartPhone.cs
@@ -0,0 +1,21 @@
+﻿namespace EventCall.Data.Models
+{
+    public class SmartPhone
+    {
+        public int Id { get; set; }
+        public string SmartPhoneName { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public string Price { get; set; }
+
+        public List<Feature> SmartPhoneFeatures { get; set; }
+    }
+
+    public class Feature
+    {
+        public string Specification { get; set; }
+
+        public string Value { get; set; }
+    }
+}
