aad3150e0707ef2cb14fbc9f588b28b03acc61bc
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2024-06-25 17:06:26 +0530

    WPF Blazor Hybrid App
    Who would have thought there would come a day when we would all witness the blend of two of the best UI frameworks: WPF and Blazor? I've had the opportunity to work with both. Now, the dawn of this era is here, and it's time to seize the opportunity. you can now harness the power of both UI frameworks and create hybrid apps. An app that runs seamlessly on both desktop and web platforms. Well, folks, that day has finally arrived. Let's get cracking.

---
+++ WPFBlazorHybrid/WPFBlazorHybrid/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WPFBlazorHybrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
