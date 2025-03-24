bb6188dc7cb48447428ec6bd22a3ca53c3cc2061
RikamPalkar <rikampalkar@gmail.com>
2025-03-24 13:22:30 +0530

    Dependency Injection

---
+++ DependencyInjectionDemo/DependencyInjectionDemo/Services/Interfaces/IWeatherService.cs
@@ -0,0 +1,9 @@
+﻿using static DependencyInjectionDemo.Pages.Weather;
+
+namespace DependencyInjectionDemo.Services.Interfaces
+{
+    public interface IWeatherService
+    {
+        Task<WeatherForecast[]> GetWeatherForecastsAsync();
+    }
+}
---
+++ DependencyInjectionDemo/DependencyInjectionDemo/Services/TransientService.cs
@@ -0,0 +1,12 @@
+﻿using DependencyInjectionDemo.Services.Interfaces;
+
+namespace DependencyInjectionDemo.Services
+{
+    public class TransientService : ITransientService
+    {
+        public string GenerateUniqueId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
