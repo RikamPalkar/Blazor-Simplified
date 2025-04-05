474d82f489830aba1ea83f3232934375cbd35d1c
Rikam <36474843+RikamPalkar@users.noreply.github.com>
2025-04-05 16:35:40 +0530

    Adding theme swtich app

---
+++ ThemeSwitch/ThemeSwitch/wwwroot/js/theme.js
@@ -0,0 +1,10 @@
+﻿function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
+}
+
+window.applySavedTheme = function () {
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+}
